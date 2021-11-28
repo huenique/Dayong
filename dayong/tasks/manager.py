c1cc7f65089b93df674548a6dd2e882882a95d4d
Hju Kneyck Flores <hjucode@gmail.com>
2021-11-28 15:17:48 +0800

    fix: prevent large increments on job interval

--- dayong/tasks/manager.py
+++ dayong/tasks/manager.py
@@ -1,7 +1,6 @@
 """
 dayong.tasks.manager
 ~~~~~~~~~~~~~~~~~~~~
-
 Custom, built-in implementation of background task managers.
 """
 import asyncio
@@ -16,10 +15,8 @@ class AioTaskManager:
 
     async def get_task(self, task_name: str) -> Task[Any]:
         """Fetch the task object for a specified task.
-
         Args:
             task_id (int): The name assigned to the task object to retrieve.
-
         Returns:
             Task[Any]: A coroutine wrapped in a Future.
         """
@@ -33,12 +30,10 @@ class AioTaskManager:
         *coro_args: Any,
     ) -> tuple[str, Task[Any]]:
         """Schedule the execution of a coroutine.
-
         Args:
             coro_fn (Callable[..., Coroutine[Any, Any, Any]]): The coroutine to execute.
             task_name (str): The name of the task to execute.
             execute_in (float): The execution time delay.
-
         Returns:
             tuple[str, Task[Any]]: The task name and `asyncio.Task` object.
         """
@@ -54,14 +49,19 @@ class AioTaskManager:
 
         loop = asyncio.get_running_loop()
         task = loop.create_task(wrapped_coro(), name=task_name)
-        self.tasks[task.get_name()] = task
+        self.tasks[task_name] = task
         return task_name, task
 
-    async def stop_task(self, task_name: str):
+    async def stop_task(self, task_name: str) -> bool:
         """Stop a task running in the background.
-
         Args:
             task_id (int): Integer associated with a task object.
         """
-        task = await self.get_task(task_name)
-        task.cancel()
+        try:
+            task = self.tasks[task_name]
+            task.cancel()
+            del self.tasks[task_name]
+        except KeyError:
+            return False
+
+        return True
